b9d02dd38c207ca934052c056b91202900c2f309
Judith Rea Nogales <judithrea98@gmail.com>
2020-10-03 21:24:13 +0200

    Api setup

--- src/api/index.js
+++ src/api/index.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 const apiClient = axios.create({
   baseURL:
-    `https://powerful-anchorage-64847.herokuapp.com/` ||
+    `https://powerful-anchorage-64847.herokuapp.com/api` ||
     "http://localhost:8080/api",
 });
 
