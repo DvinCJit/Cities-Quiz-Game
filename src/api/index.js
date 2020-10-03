351bbbea338cdaefd7c91212c54e6d151a29df71
Judith Rea Nogales <judithrea98@gmail.com>
2020-10-03 21:08:16 +0200

    Added base url

--- src/api/index.js
+++ src/api/index.js
@@ -1,7 +1,9 @@
 import axios from "axios";
 
 const apiClient = axios.create({
-  baseURL: "http://localhost:8080/api",
+  baseURL:
+    `https://powerful-anchorage-64847.herokuapp.com/` ||
+    "http://localhost:8080/api",
 });
 
 export const startgame = (data) => apiClient.post("/", data);
