8ba1a4dae182dd6235778587e56d1ca109860e27
Judith Rea Nogales <judithrea98@gmail.com>
2020-10-03 21:17:50 +0200

    Set port

--- server/index.js
+++ server/index.js
@@ -21,4 +21,6 @@ app.get("/api/cities", (req, res) => {
   res.send(cities);
 });
 
-app.listen(port, () => console.log(`Server running on port ${port}`));
+app.listen(process.env.PORT || port, () =>
+  console.log(`Server running on port ${port}`)
+);
